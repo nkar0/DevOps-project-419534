7995b607d336add4551cedef84d48f81d77a219e
Natalia <159160593+nkar0@users.noreply.github.com>
2026-06-18 08:02:13 +0200

    Update message in /demo endpoint response

--- app/app.py
+++ app/app.py
@@ -19,7 +19,7 @@ class AppHandler(BaseHTTPRequestHandler):
             status_code = 200
         elif self.path == "/demo":
             response = {
-                "message": "Nowy endpoint wdrozony automatycznie",
+                "message": "Prezentacja",
                 "student": "419534"
             }
             status_code = 200
